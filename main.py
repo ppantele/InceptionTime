a2efeb05d5d4566fd0e745344ce4215a535b8554
ppantele <ppantele@users.noreply.github.com>
2021-04-17 22:23:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def get_xp_val(xp):
 
 
 ############################################### main
-root_dir = '/content/models/IncTime/'
+root_dir = '/content/models/IncTime'
 xps = ['use_bottleneck', 'use_residual', 'nb_filters', 'depth',
        'kernel_size', 'batch_size']
 
